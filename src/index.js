2ea678e3c477fcc413de50741923368426952e03
ZhannaIvashova <zhannaklepikova90@gmail.com>
2024-08-13 15:20:21 +0300

    feat: add algorithm task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const result = [];
+  if (!matrix) return result
+  for(let item = 0; item < matrix.length; item ++) {
+      if (item % 2 === 0) {
+          result.push(...matrix[item])
+      } else {
+          result.push(...matrix[item].reverse())
+      }
+  }
+  return result
 }
